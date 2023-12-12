302b2ae47660c94875dd7bda1afe21115eb56f6d
Mihai Paul Orban <144853573+mihaipaulorban@users.noreply.github.com>
2023-12-12 15:30:15 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'DefaultTemplate',
+};
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use './assets/scss/partials/variables' as *;
+@use './assets/scss/partials/common ' as *;
+@use './assets/scss/partials/reset' as *;
+</style>
